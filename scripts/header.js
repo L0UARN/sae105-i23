4040d71239e1268abbea0c95e4f4f2ea398faf02
Louarn <louarn.richard@protonmail.com>
2022-01-16 10:50:47 +0100

    Réglage d'un problème avec le nav (les boutons étaient coupés si ont réduisait la largeur de la fenêtre)

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,6 @@
 window.addEventListener('load', () => {
     let ouvert = false;
     let menu = document.getElementById('nav-menu');
-    menu.style.overflow = 'hidden';
-    menu.style.height = '3.5em';
 
     document.getElementById('burger-activator').addEventListener('click', () => {
         ouvert = !ouvert;
